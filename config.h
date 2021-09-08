101123f025a5d890683e1f1681d2e5c2b84169ed
Frank Salas <frankrogerstg@gmail.com>
2021-09-08 17:45:09 -0500

    Bar height patch applied

--- config.h
+++ config.h
@@ -10,8 +10,10 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int user_bh            = 20;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+
+static const char *fonts[]          = { "Hack:size=9" };
+static const char dmenufont[]       = "Hack:size=11";
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
